1fb45f9b0e273f1d5aec82aeecf491973da621de
AbdallhOmarr <abdallhomarr.com>
2023-04-05 14:43:29 +0200

    شسيشس

--- vrp_problem.py
+++ vrp_problem.py
@@ -10,6 +10,120 @@ import re
 from Route import *
 from Solution import * 
 from sklearn.cluster import KMeans
+import copy
+from scipy.cluster.hierarchy import linkage, fcluster
+import numpy as np
+# def load_data():
+#     # lets load data
+#     txt_file = "vrp data.txt"
+#     data = []
+#     with open(txt_file, "r") as file:
+#         lines = file.readlines()  # Read all lines in the file
+#         column_parsed = False
+#         column_names = []
+#         if column_parsed == False:
+#             # Extract column names from first line
+#             words = re.findall("\S+", lines[0])
+#             for word in words:
+#                 column_names.append(word)
+#             column_parsed = True
+
+#         for line in lines[1:]:  # Loop through lines, skipping the first line
+#             # Extract data points from line
+#             dataline = re.findall("\S+", line)
+#             line_dict = {}
+#             for i, point in enumerate(dataline):
+#                 # Convert data point to float and store in dictionary
+#                 line_dict[column_names[i]] = float(point)
+#             data.append(line_dict)  # Append dictionary to list of data
+       
+
+
+#     #converting list of dict to numpy array
+#     data_array = np.array([[d[col] for col in column_names] for d in data])
+
+#     #extract first row into depot array
+#     DEPOT = data_array[0, :]
+
+#     # extract rest of the rows into customers_array
+#     customers_array = data_array[1:, :]
+#     customers_array = add_distance_feature(customers_array,DEPOT)
+
+#     return customers_array,DEPOT
+
+
+# #getting distance from depot for all points 
+# def get_distance_between_two_points(x1,y1,x2,y2):
+#     x_diff = x2-x1
+#     y_diff = y2-y1
+#     dist = math.sqrt(math.pow(x_diff, 2) + math.pow(y_diff, 2))
+#     return dist
+
+# def add_distance_feature(customers_array, depot):
+#     # extract XCOORD and YCOORD columns from customers array
+#     XCOORD = customers_array[:, 1]
+#     YCOORD = customers_array[:, 2]
+    
+#     # calculate distances from depot to customers
+#     distances = np.sqrt((XCOORD - depot[1])**2 + (YCOORD - depot[2])**2)
+    
+#     # add distances as new column to customers array
+#     customers_array = np.column_stack((customers_array, distances))
+    
+#     return customers_array
+
+# def cluster_array(customers_array, n_clusters):
+   
+#     # # extract columns to cluster from customers array
+#     # columns_to_cluster = [1,2,4]  # indices of READY_TIME, DUE_DATE, XCOORD, YCOORD, and distance_FROM_DEPOT columns
+    
+#     # # normalize the data
+#     # data_norm = (customers_array[:, columns_to_cluster] - customers_array[:, columns_to_cluster].mean(axis=0)) / customers_array[:, columns_to_cluster].std(axis=0)
+    
+#     # # cluster the data using KMeans
+#     # kmeans = KMeans(n_clusters=n_clusters).fit(data_norm)
+    
+#     # # add cluster labels to customers array
+#     # customers_array = np.column_stack((customers_array, kmeans.labels_))
+    
+#     # # sort points by cluster and ready time
+#     # sorted_indices = np.lexsort((customers_array[:, 5], customers_array[:, 8]))
+#     # customers_array = customers_array[sorted_indices, :]
+    
+#     # # # sort points by distance from depot within each cluster
+#     # # for i in range(n_clusters):
+#     # #     cluster_indices = np.where(customers_array[:, -1] == i)[0]
+#     # #     sorted_indices = np.argsort(customers_array[cluster_indices, 7])
+#     # #     customers_array[cluster_indices] = customers_array[cluster_indices][sorted_indices]
+        
+        
+#     # Extract columns to cluster from customers array
+#     columns_to_cluster = [1, 2, 7]  # indices of XCOORD, YCOORD, and distance_FROM_DEPOT columns
+
+#     # Normalize the data
+#     data_norm = (customers_array[:, columns_to_cluster] - customers_array[:, columns_to_cluster].mean(axis=0)) / customers_array[:, columns_to_cluster].std(axis=0)
+
+#     # Use HAC to cluster the data
+#     Z = linkage(data_norm, method='ward')
+
+#     # Determine cluster labels for each data point
+#     max_distance = 2.5  # maximum distance to consider when clustering
+#     clusters = fcluster(Z, t=max_distance, criterion='distance')
+
+#     # Add cluster labels to customers array
+#     customers_array = np.column_stack((customers_array, clusters))
+
+#     # Sort points by cluster and ready time
+#     sorted_indices = np.lexsort((customers_array[:, 4], customers_array[:, 8]))
+#     customers_array = customers_array[sorted_indices, :]
+
+#     return customers_array
+
+
+import numpy as np
+from scipy.cluster.hierarchy import fcluster, linkage
+import math
+import re
 
 def load_data():
     # lets load data
@@ -50,24 +164,32 @@ def load_data():
     return customers_array,DEPOT
 
 
-#getting distance from depot for all points 
-def get_distance_between_two_points(x1,y1,x2,y2):
-    x_diff = x2-x1
-    y_diff = y2-y1
+def get_distance_between_two_points(x1, y1, x2, y2):
+    x_diff = x2 - x1
+    y_diff = y2 - y1
     dist = math.sqrt(math.pow(x_diff, 2) + math.pow(y_diff, 2))
     return dist
 
+
 def add_distance_feature(customers_array, depot):
-    # extract XCOORD and YCOORD columns from customers array
+    # Extract XCOORD and YCOORD columns from customers array
     XCOORD = customers_array[:, 1]
     YCOORD = customers_array[:, 2]
     
-    # calculate distances from depot to customers
+    # Calculate distances from depot to customers
     distances = np.sqrt((XCOORD - depot[1])**2 + (YCOORD - depot[2])**2)
     
-    # add distances as new column to customers array
+    # Add distances as new column to customers array
     customers_array = np.column_stack((customers_array, distances))
     
+    return customers_array
+
+
+def add_time_window_feature(customers_array):
+    # Calculate the amount of time available to service each customer
+    time_window = customers_array[:, 5] - customers_array[:, 4]
+    customers_array = np.column_stack((customers_array, time_window))
+
     return customers_array
 
 def cluster_array(customers_array, n_clusters):
@@ -94,12 +216,46 @@ def cluster_array(customers_array, n_clusters):
     #     sorted_indices = np.argsort(customers_array[cluster_indices, 7])
     #     customers_array[cluster_indices] = customers_array[cluster_indices][sorted_indices]
     
-    return customers_array
+    return customers_array,n_clusters
 
 
 
+# def cluster_array(customers_array, n_clusters):
+#     customers_array = add_time_window_feature(customers_array)
+#     # Extract columns to cluster from customers array
+#     columns_to_cluster = [1, 2, 4, 5, 6, 7]  # indices of XCOORD, YCOORD, READY_TIME, DUE_DATE, distance_FROM_DEPOT, and time window columns
 
-        
+#     # Normalize the data
+#     eps = 1e-6
+#     data_norm = (customers_array[:, columns_to_cluster] - customers_array[:, columns_to_cluster].mean(axis=0)) / (customers_array[:, columns_to_cluster].std(axis=0) + eps)
+#     # Check for NaN values
+#     # print(np.isnan(data_norm).any())
+
+#     # Check for infinite values
+#     # print(np.isfinite(data_norm).all())
+
+#     # Use HAC to cluster the data
+#     Z = linkage(data_norm, method='ward')
+
+#     # Determine cluster labels for each data point
+#     max_distance = 3  # maximum distance to consider when clustering
+#     clusters = fcluster(Z, t=max_distance, criterion='distance')
+#     n_clusters = len(np.unique(clusters))
+    
+#     # Add cluster labels to customers array
+#     customers_array = np.column_stack((customers_array, clusters))
+
+#     # Sort points by cluster, ready time, and due date
+#     sorted_indices = np.lexsort((customers_array[:, 9], customers_array[:, 4], customers_array[:, 5]))
+#     customers_array = customers_array[sorted_indices, :]
+
+#     # Sort points by distance from depot within each cluster
+#     for i in range(n_clusters):
+#         cluster_indices = np.where(customers_array[:, -1] == i)[0]
+#         sorted_indices = np.argsort(customers_array[cluster_indices, 8])
+#         customers_array[cluster_indices] = customers_array[cluster_indices][sorted_indices]
+
+#     return customers_array,n_clusters
 
 
 
@@ -110,16 +266,10 @@ def cluster_array(customers_array, n_clusters):
 
 
 def get_routes(customers_array,n_clusters,DEPOT):
-
     routes =[]
-    for n in range(n_clusters):
+    for n in range(1,n_clusters):
         route_customers = customers_array[customers_array[:, 8] == n]
-  
         if route_customers.shape[0]==0:
-            print(f"n:{n}")
-            print("-"*50)
-            print(route_customers)
-            print("-"*50)
             continue
 
         route = Route(route_customers,DEPOT)
@@ -131,7 +281,7 @@ def generate_initial_population(customers_array,POPULATION_SIZE,n_cluster,DEPOT)
 
     population = []
     for i in range(POPULATION_SIZE):
-        clustered_customers_array = cluster_array(customers_array,n_cluster)
+        clustered_customers_array,n_cluster = cluster_array(customers_array,n_cluster)
         routes = get_routes(clustered_customers_array,n_cluster,DEPOT)
 
         #error area
@@ -160,18 +310,17 @@ def create_offspring(parent1,parent2,crossover_rate):
     if random.random() > crossover_rate:
         # if not, return a clone of parent1 or parent2 at random
         if random.random() < 0.5:
-            return parent1
+            return copy.deepcopy(parent1)
         else:
-            return parent2
-        
+            return copy.deepcopy(parent2)
+    
     p1_routes = parent1.routes
     p2_routes = parent2.routes
-
     crossover_point = random.randint(1,len(p1_routes)-1)
 
     offspring_routes = p1_routes[:crossover_point] + p2_routes[crossover_point:]
     offspring_sol = Solution(offspring_routes)
-    offspring_sol.prevent_duplicated_customers()
+    # offspring_sol.prevent_duplicated_customers()
     offspring_sol.update_variables()
     return offspring_sol
 
